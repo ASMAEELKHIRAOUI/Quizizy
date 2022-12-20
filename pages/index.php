e8c1b1fd4cc3b40a6d221960962e56eec12531db
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2022-12-20 19:36:34 +0100

    questions page done working on the homepage

--- pages/index.php
+++ pages/index.php
@@ -9,38 +9,38 @@
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/sass/style.css"/>
+    <link rel="stylesheet" href="../assets/sass/style.css"/>
     <link rel="stylesheet" href="https://parsleyjs.org/src/parsley.css"/>
     <title>Quizizy</title>
 </head>
 <body>
     <div class="row-lg h-100 d-flex align-items-center p-5 flex-wrap">
         <div class="col-lg-1"></div>
-        <div class="col-lg-9">
-            <div class="d-flex justify-content-center my-5">
+        <div class="col-lg-10">
+            <div class="qst d-flex justify-content-center">
                 Why is AWS more economical than traditional data centers for applications with varying compute
                 workloads?
             </div>
-            <div class="d-flex flex-column align-items-center">
-                <ul class="d-flex justify-content-between">
-                    <li>
-                        Amazon EC2 costs are billed on a monthly basis
-                    </li>
-                    <li>
-                        Amazon EC2 costs are billed on a monthly basis
-                    </li>
-                </ul>
-                <div class="number">
+            <div class="choices row-lg h-100 d-flex align-items-center p-5 flex-wrap">
+                <div class="col-lg-5">
+                    <div class="choice1">
+                        Amazon EC1 costs are billed on a monthly basis 
+                    </div>
+                    <div class="choice2">
+                        Amazon EC2 costs are billed on a monthly basis 
+                    </div>
+                </div>
+                <div class="number col-lg-2">
                     1
                 </div>
-                <ul class="d-flex justify-content-between">
-                    <li>
-                        Amazon EC2 costs are billed on a monthly basis
-                    </li>
-                    <li>
-                        Amazon EC2 costs are billed on a monthly basis
-                    </li>
-                </ul>
+                <div class="col-lg-5">
+                    <div class="choice3">
+                        Amazon EC3 costs are billed on a monthly basis 
+                    </div>
+                    <div class="choice4">
+                        Amazon EC4 costs are billed on a monthly basis 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
